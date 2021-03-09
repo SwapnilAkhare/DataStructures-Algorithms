013d7903c21a227d2c44e74937662df24abea5ff
SwapnilAkhare <swapakhare98@gmail.com>
2021-03-09 19:19:05 +0530

    Next Greater Integer

---
+++ DataStructures&Algorithms/src/Puzzle/NextGreaterInteger.java
@@ -0,0 +1,47 @@
+package Puzzle;
+
+public class NextGreaterInteger {
+
+    // find out next greater integer in O(n) complexity
+
+    public static void main(String[] args) {
+
+        int num = 1318540;
+        System.out.println(nextGreater(num));
+    }
+
+    private static int nextGreater(int num) {
+
+        char[] digits = String.valueOf(num).toCharArray();
+
+        int i = digits.length - 2;
+
+        while(i>=0 && digits[i]>=digits[i+1])
+        {
+            i--;
+        }
+        if(i == -1)
+            return num;
+
+        int j = digits.length-1;
+        while (digits[j]<=digits[i]) {
+            j--;
+        }
+
+       char temp = digits[j];
+        digits[j] = digits[i];
+        digits[i] = temp;
+
+         int Left= i + 1, Right = digits.length-1;
+         while(Left < Right){
+
+              temp = digits[Left];
+              digits[Left] = digits[Right];
+              digits[Right] = temp;
+             Left++;
+             Right--;
+         }
+
+         return Integer.parseInt(String.valueOf(digits));
+    }
+}
