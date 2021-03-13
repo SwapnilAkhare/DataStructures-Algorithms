8531654cab776922f8d0e09d4803cd76ce9ebf9d
SwapnilAkhare <swapakhare98@gmail.com>
2021-03-13 08:59:51 +0530

    Zero sum array with O(n^2) and O(n) solution

--- DataStructures&Algorithms/src/Array/zeroSumArray.java
+++ DataStructures&Algorithms/src/Array/zeroSumArray.java
@@ -1,16 +1,38 @@
 package Array;
 
 // given an integer array ,check if subarray give sum zero
-
+// TIME COMPLEXITY O(N^2)
 // Time complexity O(n)
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class zeroSumArray {
+ // TIME COMPLEXITY O(N^2)
+// function to check subarray with zero sum with O(n^2) COMPLEXITY
+    public static void checkArrayBruteForce(int[] A){
 
+        //traverse i from start to end
+        for(int i = 0; i < A.length;i++){
+
+            int sum = 0;
+
+            for(int j=i; j<A.length; j++){
+
+                sum = sum + A[j];
+
+                        // if thre sum is found before we have found thre array
+                if(sum == 0) {
+                    System.out.println("subarray"+i + ".." + j);
+                }
+            }
+        }
+
+    }
+
+    // TIME COMPLEXITY O(N)
     // function to check subaarray with given zero sum
-    public static boolean checkArray(int[] A){
+    public static boolean checkArraySet(int[] A){
 
 
         // cretate an empty set to store sum of each
@@ -24,6 +46,7 @@ public class zeroSumArray {
           int sum = 0;
 
           //traverse the given array
+        // for every value in array do following
         for(int value : A){
 
             // sum of element so fAR
@@ -47,12 +70,13 @@ public class zeroSumArray {
 
         int[] A = {4,-6,3,-1,4,2,7};
 
-        if(checkArray(A)){
+        if(checkArraySet(A)){
             System.out.println("subarray exist");
 
         }else{
 
             System.out.println("doesn't exist");
         }
+        checkArrayBruteForce(A);
     }
 }
