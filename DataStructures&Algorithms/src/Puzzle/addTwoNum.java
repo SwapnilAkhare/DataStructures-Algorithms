e1ec8c6df307bc3d74369ce0d18779b376cfb0a1
SwapnilAkhare <swapakhare98@gmail.com>
2021-03-19 03:07:56 +0530

    extra method to add 2 num w/o addition operator

--- DataStructures&Algorithms/src/Puzzle/addTwoNum.java
+++ DataStructures&Algorithms/src/Puzzle/addTwoNum.java
@@ -1,21 +1,45 @@
 /* ADD TWO NUMBER WITHOUT USING ADDITION OPERATOR
-
  */
 
 package Puzzle;
 
 import java.util.Scanner;
 
-public class addTwoNum {
+
+import static java.lang.Math.log;
+import static java.lang.Math.exp;
+
+public class addTwoNum{
     public static void main(String[] args) {
-        int a,b,c;
+        double a,b,c;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number to be added :");
         a=sc.nextInt();
         b=sc.nextInt();
-        c=a -(-b);
+        //using substraction operator
+        c=a-(-b);
      System.out.println(c);
+     // using logarithm
+     System.out.println(log(exp(a) * exp(b)));
+
+     // we can use repeated addition/ substraction operator
+      while(a>0){
+          b++;
+          a--;
+      }
+      while(a<0){
+          a++;
+          b--;
+
+      }
+      System.out.println(b);
+
+
     }
-
-
 }
+
+
+
+
+
+
