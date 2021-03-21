d8d7b6fd62fcb0d85e55ef13f810d6aec59d111c
SwapnilAkhare <swapakhare98@gmail.com>
2021-03-21 23:54:48 +0530

    Semicolon without using semicolon

---
+++ DataStructures&Algorithms/src/Puzzle/SemicolonWithoutSemicolon.java
@@ -0,0 +1,25 @@
+
+// print semicolon without using semicolon ?
+// the idea here is to use Ascii value of semicolon which is 59
+
+package Puzzle;
+
+public class SemicolonWithoutSemicolon {
+    public static void main(String[] args) {
+
+        // if(printf("%c",59)){} || if (putchar(59)){}......in C language
+
+        // here we use printf becoz it return printstream.... and println return void
+        // we can also use... append()..in place of printf()
+        if(System.out.printf("%c", 59)==null) {
+
+            //empty body to avoid use of semicolon
+        }
+
+        // while (!printf(59))...in c lang
+        // using while
+        while(System.out.printf("%c", 59) == null){
+
+        }
+    }
+}
