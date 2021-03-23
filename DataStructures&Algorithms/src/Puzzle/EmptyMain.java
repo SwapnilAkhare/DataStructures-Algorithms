26ffcbb016c12c1f4c6568587be411527398d160
SwapnilAkhare <swapakhare98@gmail.com>
2021-03-23 21:31:05 +0530

    print statement without main() function

---
+++ DataStructures&Algorithms/src/Puzzle/EmptyMain.java
@@ -0,0 +1,14 @@
+
+// print hello world with empty main() function
+
+package Puzzle;
+
+public class EmptyMain {
+
+    // static block is use to print anything without main()
+    static
+    {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args){}
+}
