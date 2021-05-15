b6499240660ef1d12bd7ae5f0e7befb385142732
SwapnilAkhare <swapakhare98@gmail.com>
2021-05-15 20:11:21 +0530

    Half Pyramid 180 rotation

---
+++ DataStructures&Algorithms/src/Patterns/HalfPyramid180Rotation.java
@@ -0,0 +1,30 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HalfPyramid180Rotation {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+
+            for(int j=1; j<=n; j++){
+
+                if(j<=n-i){
+
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+
+
+    }
+}
