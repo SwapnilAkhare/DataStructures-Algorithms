7a5930f8ef64e6aa6778eefc823fd36979264aa6
SwapnilAkhare <swapakhare98@gmail.com>
2021-05-17 15:56:35 +0530

    prime or not

--- DataStructures&Algorithms/src/UseOfContinueAndBreak/PrimeOrNot.java
+++ DataStructures&Algorithms/src/UseOfContinueAndBreak/PrimeOrNot.java
@@ -1,16 +1,15 @@
-package Practice;
+package UseOfContinueAndBreak;
 
 import java.util.Scanner;
 
 public class PrimeOrNot {
     public static void main(String[] args){
-
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int i;
-        for( i=2;i<n;i++){
-            if(n%i==0){
-                System.out.println("Not Prime");
+        for(i=2;i<n;i++){
+            if(n%i==0) {
+                System.out.println("not prime");
                 break;
             }
         }
