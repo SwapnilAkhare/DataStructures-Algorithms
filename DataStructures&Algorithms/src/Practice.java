6346cb840f30eb0b8d05e341815786ca4fee47f1
SwapnilAkhare <swapakhare98@gmail.com>
2021-05-17 15:48:38 +0530

    prime or not

--- DataStructures&Algorithms/src/Practice.java
+++
@@ -1,9 +0,0 @@
-public class Practice {
-    public static void main(String[] args) {
-
-       if(System.out.printf( "%c" ,59 )== null){
-
-       }
-    }
-
-}
---
+++ DataStructures&Algorithms/src/Practice/PrimeOrNot.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i;
+        for( i=2;i<n;i++){
+            if(n%i==0){
+                System.out.println("Not Prime");
+                break;
+            }
+        }
+        if(i==n){
+            System.out.println("is prime");
+        }
+    }
+}
