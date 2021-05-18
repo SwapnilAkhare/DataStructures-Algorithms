227d12d70f4a4a1f0b152c6aecf1e849ea675a63
SwapnilAkhare <swapakhare98@gmail.com>
2021-05-18 09:49:30 +0530

    Reverse a number

---
+++ DataStructures&Algorithms/src/Practice/ReverseNo.java
@@ -0,0 +1,23 @@
+package Practice;
+//reverse a no example 234.... 432
+
+import java.util.Scanner;
+
+public class ReverseNo {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+
+        int reverse=0;
+        while(n>0){
+            int lastdigit=n%10;
+            reverse=reverse *10 + lastdigit;
+            n=n/10;
+        }
+        System.out.println(reverse);
+
+
+    }
+}
