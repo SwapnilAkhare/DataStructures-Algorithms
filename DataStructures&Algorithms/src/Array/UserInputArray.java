a4db7dfb9b2a18c98fc3c741f2476bb6eb6fc012
SwapnilAkhare <swapakhare98@gmail.com>
2021-05-19 21:59:54 +0530

    Create User Input Array

---
+++ DataStructures&Algorithms/src/Array/UserInputArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class UserInputArray {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int size = sc.nextInt();
+
+        int[] arr=new int[size];  // array declarartion in c/cpp is.. int arr[]
+
+        for(int i=0;i<size;i++){
+
+            arr[i]=sc.nextInt(); // read input from user
+
+            System.out.print(arr[i]);
+
+            System.out.print(" ");
+        }
+    }
+}
